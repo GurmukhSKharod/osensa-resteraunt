ed7fc6b8d5ecdf5a4693e22b5cd141a331bfb0b8
GurmukhSKharod <gurmukhkharod@hotmail.com>
2025-10-27 11:13:08 -0700

    fix: broker docker fixes.

--- broker/start.sh
+++ broker/start.sh
@@ -11,8 +11,7 @@ listener ${PORT}
 protocol websockets
 http_dir /mosquitto/www
 
-# Demo-friendly; tighten for prod with auth (see notes later)
 allow_anonymous true
 EOF
 
-exec /docker-entrypoint.sh mosquitto -c /mosquitto/config/mosquitto.conf
+exec /usr/sbin/mosquitto -c /mosquitto/config/mosquitto.conf -v
